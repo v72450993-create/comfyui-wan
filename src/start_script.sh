05754b4ce1d18c3b4ec738af9ddd2a79142525bb
Sander van Leeuwen <replytosander@gmail.com>
2026-03-25 14:43:18 +0100

    Check if github available before continuing

--- src/start_script.sh
+++ src/start_script.sh
@@ -1,3 +1,12 @@
-git clone https://github.com/Hearmeman24/comfyui-wan.git
+#!/bin/bash
+
+# 1. Wait for the network to be ready
+echo "Waiting for internet..."
+until curl -s --head https://github.com > /dev/null; do
+  sleep 2
+done
+
+git clone https://github.com/v72450993-create/comfyui-wan.git
+
 mv comfyui-wan/src/start.sh /
 bash /start.sh
